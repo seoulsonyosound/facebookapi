b1c3840981629523d81957b6da25353a54dee6f0
JoshuaParungao <jmphotography1830@gmail.com>
2025-11-04 13:58:40 +0800

    first commit

---
+++ src/main/java/com/alejos/facebookapi/PostRepository.java
@@ -0,0 +1,8 @@
+package com.alejos.facebookapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
