c2e77c1060821375a0c7486fd026aa71f5fef4d7
seoulsonyosound <alejostheeannajether@gmail.com>
2025-11-06 12:31:16 +0800

    added cross origin

--- src/main/java/com/alejos/facebookapi/PostController.java
+++ src/main/java/com/alejos/facebookapi/PostController.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
